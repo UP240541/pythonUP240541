ac80e7490d1c031d467d5ec352d2c76503704201
UP240541 <up240541@alumnos.upa.edu.mx>
2025-03-19 10:31:31 -0600

    Dia9_01&02

--- Ejercicios_python/09_Day/01_Exercises.py
+++ Ejercicios_python/09_Day/01_Exercises.py
@@ -5,15 +5,53 @@
 #Output:
 #Enter your age: 15
 #You need 3 more years to learn to drive.
+print("Ejercicio 1")
+age = int(input("Ingrese su edad: "))
+if age > 17:
+    print("Tienes la edad para conducir")
+else:
+    si = 18 - age
+    print("Te faltan", si, "para aprender a conducir")
 
 #2 Compare the values of my_age and your_age using if … else. Who is older (me or you)? Use input(“Enter your age: ”) to get the age as input.
-#  You can use a nested condition to print 'year' for 1 year difference in age, 'years' for bigger differences,
-#  and a custom text if my_age = your_age. Output:
-#Enter your age: 30
-#You are 5 years older than me.
+# You can use a nested condition to print 'year' for 1 year difference in age, 'years' for bigger differences,
+# and a custom text if my_age = your_age. Output:
+# Enter your age: 30
+# You are 5 years older than me.
+print("Ejercicio 2")
+myAge = int(input("Ingrese su edad: "))
+yourAge = int(input("Ingrese la edad de la otra persona: "))
+if myAge > yourAge:
+    print("Yo soy mayor")
+    dif = myAge - yourAge
+    if dif == 1:
+        print("La diferencia de edad es de un año")
+    else:
+        print("Yo soy ", dif, " años mayor que tu")
+
+elif yourAge > myAge:
+    print("Tu eres mayor")    
+    dif = yourAge - myAge
+    if dif == 1:
+        print("La diferencia de edad es de un año")
+    else:
+        print("Tu eres ", dif, " años mayor que yo")
+
+elif myAge == yourAge:
+    print("Tenemos la misma edad")
+
 
 #3 Get two numbers from the user using input prompt. If a is greater than b return a is greater than b,
 #  if a is less b return a is smaller than b, else a is equal to b. Output:
 #Enter number one: 4
 #Enter number two: 3
 #4 is greater than 3
+print("Ejercicio 3")
+a = int(input("Ingrese un numero: "))
+b = int(input("Ingrese un segundo numero: "))
+if a > b:
+    print(a," es mayor que ", b)
+elif b > a:    
+    print(b," es mayor que ", a)
+elif a == b:
+    print(a, " y ", b," son iguales")    
--- Ejercicios_python/09_Day/02_Exercises.py
+++ Ejercicios_python/09_Day/02_Exercises.py
@@ -1,4 +1,3 @@
-
 #Exercises: Level 2
 #1 Write a code which gives grade to students according to theirs scores:
 #80-100, A
@@ -6,9 +5,34 @@
 #60-69, C
 #50-59, D
 #0-49, F
+calf = int(input("Ingrese su calificacion: "))
+if calf >= 90 and calf <= 100:
+    print("Tu calificacion de", calf, "equivale a una A")
+elif calf >= 70 and calf <= 89:   
+    print("Tu calificacion de", calf, "equivale a una B")
+elif calf >= 60 and calf <= 69:   
+    print("Tu calificacion de", calf, "equivale a una C")
+elif calf >= 50 and calf <= 59:   
+    print("Tu calificacion de", calf, "equivale a una D")
+elif calf >= 0 and calf <= 49:   
+    print("Tu calificacion de", calf, "equivale a una F")   
+else:
+    print("Error")     
 
-#2 Check if the season is Autumn, Winter, Spring or Summer. If the user input is: September, October or November, the season is Autumn. December, January or February, the season is Winter. 
+#2 Check if the season is Autumn, Winter, Spring or Summer. If the user input is: September, October or November, the season is Autumn. 
+# December, January or February, the season is Winter. 
 # March, April or May, the season is Spring June, July or August, the season is Summer
+season = str(input("Ingrese un mes: "))
+if season in ["Septiembre","Octubre", "Noviembre"]:
+    print(season, "esta en otoño")
+elif season in ["Diciembre", "Enero", "Febrero"]:
+    print(season, "esta en invierno")
+elif season in ["Marzo", "Abril", "Mayo"]:
+    print(season, "esta en primavera")
+elif season in ["Junio", "Julio", "Agosto"]:
+    print(season, "esta en verano")
+else:
+    print("Error")     
 
 
 #3 The following list contains some fruits:
@@ -17,3 +41,10 @@
 #```
 # If a fruit doesn't exist in the list add the fruit to the list and print the modified list.
 #  If the fruit exists print('That fruit already exist in the list')
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruta = str(input("Ingrese una fruta: "))
+if fruta in fruits:
+    print("Esa fruta ya exite")
+else:
+    fruits.append(fruta)
+    print(fruits)
